161b9a8879e0b5e690627560d80f322f78aa8f16
tarrinros <tarindis@gmail.com>
2019-04-03 00:48:22 +0300

    Add night_quest scenarion in json format

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 # Считывать из него уровни
 # Возвращать массив хэшей уровней
-# Когда игрок выбирает одно из значений - переходить на следующий уровень
+# Когда игрок выбирает одно из значений - переходит на следующий уровень
 
 require 'json'
 
@@ -11,13 +11,23 @@ NIGHT_QUEST_URL = 'data/night_quest.json'
 # Программа должна загружать файл вступления формата JSON +
 intro_data = JSON.parse(File.read(INTRO_URL))
 
-puts intro_data['title']
-puts
-puts "#{intro_data['text'] + intro_data['hint']}"
+puts intro_data['title'].upcase
+puts '=============================================='
+puts "#{intro_data['text']} #{intro_data['hint']}"
+puts '=============================================='
+puts 'Для того, чтобы подтвердить выбор, введите "1" и нажмите Enter: '
 
-# Во вступлении считывать системное время и загружать соответствующий json файл уровней
-quest_file_url = DAY_QUEST_URL if (6..20).cover?(Time.now.hour) | NIGHT_QUEST_URL
+time_choice = 0
 
+while time_choice != 1
+  time_choice = STDIN.gets.to_i
+  if time_choice != 1
+    puts 'Вы ввели неправильное значение'
+  end
+end
+
+# Во вступлении считывать системное время и загружать соответствующий json файл уровней + Time.now.hour
+quest_file_url = (6..20).cover?(Time.now.hour) ? DAY_QUEST_URL : NIGHT_QUEST_URL
 quest_data = JSON.parse(File.read(quest_file_url))
 
-puts quest_data['title']
+puts quest_data['daytime_choice']
